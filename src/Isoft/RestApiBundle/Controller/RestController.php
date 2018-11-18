102f628999718a6aee827e88f66477e888dc1d45
Lucian-Ionut <lucian.turiac@zitec.com>
2018-11-18 21:12:06 +0200

    Logging

--- src/Isoft/RestApiBundle/Controller/RestController.php
+++ src/Isoft/RestApiBundle/Controller/RestController.php
@@ -7,16 +7,26 @@ use Isoft\RestApiBundle\Entity\Customer;
 use Isoft\RestApiBundle\Entity\Transaction;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\Get;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class RestController extends FOSRestController
 {
     /**
      * @Get("/customer/add/{name}/{cnp}")
      */
-    public function addCustomerAction(Request $request)
+    public function addCustomerAction(Request $request, LoggerInterface $logger)
     {
         $data['name'] = $request->get('name');
         $data['cnp'] = $request->get('cnp');
+
+        if (empty($data['name']) || empty($data['cnp'])) {
+            $logger->error('Expected name and cnp not: ' . $data['name'] . 'and' . $data['cnp']);
+            throw new BadRequestHttpException
+            (
+                'Expected name and cnp not: ' . $data['name'] . 'and' . $data['cnp']
+            );
+        }
 
         $customer = new Customer();
         $customer->setName($data['name'])
@@ -27,16 +37,27 @@ class RestController extends FOSRestController
         $em->flush();
 
         $view = $this->view(['customerId' => $customer->getCustomerId()], 200);
+        $logger->info('Customer added with success!');
+
         return $this->handleView($view);
     }
 
     /**
      * @Get("/transaction/get/{customerId}/{transactionId}")
      */
-    public function getTransactionAction(Request $request)
+    public function getTransactionAction(Request $request, LoggerInterface $logger)
     {
         $data['customerId'] = $request->get('customerId');
         $data['transactionId'] = $request->get('transactionId');
+
+        if (empty($data['customerId']) || empty($data['transactionId'])) {
+            $logger->error('Expected name and cnp not: ' . $data['customerId'] . 'and' . $data['transactionId']);
+            throw new BadRequestHttpException(
+                'Expected customerId and transactionId not: ' . $data['customerId'] . 'and' . $data['transactionId']
+            );
+
+
+        }
 
         /**
          * @var \Isoft\RestApiBundle\Entity\Transaction $transaction
@@ -50,51 +71,71 @@ class RestController extends FOSRestController
                 ]
             );
 
-        $view = $this->view(
-            [
-                'transactionId' => $data['transactionId'],
-                'amount' => $transaction->getAmount(),
-                'date' => $transaction->getDate()
-            ], 200
-        );
+        $output = [
+            'transactionId' => $data['transactionId'],
+            'amount' => $transaction->getAmount(),
+            'date' => $transaction->getDate()
+        ];
+
+        $view = $this->view($output, 200);
+        $logger->info('Get transaction success!');
         return $this->handleView($view);
     }
 
     /**
      * @Get("/transaction/add/{customerId}/{amount}")
      */
-    public function addTransactionAction(Request $request)
+    public function addTransactionAction(Request $request, LoggerInterface $logger)
     {
         $data['customerId'] = $request->get('customerId');
         $data['amount'] = $request->get('amount');
+        $data['date'] = date_create(date('Y-m-d H:i:s'));
+
+        if (empty($data['customerId']) || empty($data['amount'])) {
+            $logger->error('Expected name and cnp not: ' . $data['customerId'] . 'and' . $data['amount']);
+            throw new BadRequestHttpException
+            (
+                'Expected customerId and transactionId not: ' . $data['customerId'] . 'and' . $data['amount']
+            );
+        }
 
         $transaction = new Transaction();
         $transaction->setCustomerId($data['customerId'])
             ->setAmount($data['amount'])
+            ->setDate($data['date'])
             ->setType(1);
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($transaction);
         $em->flush();
 
-        $view = $this->view(
-            [
-                'transactionId' => $transaction->getTransactionId(),
-                'customerId' => $data['customerId'],
-                'amount' => $data['amount'],
-                'date' => $transaction->getDate()
-            ], 200
-        );
+        $output = [
+            'transactionId' => $transaction->getTransactionId(),
+            'customerId' => $data['customerId'],
+            'amount' => $data['amount'],
+            'date' => $transaction->getDate()
+        ];
+
+        $view = $this->view($output, 200);
+        $logger->info("Add transaction success!" . PHP_EOL . $output);
         return $this->handleView($view);
     }
 
     /**
      * @Get("/transaction/update/{transactionId}/{amount}")
      */
-    public function updateTransactionAction(Request $request)
+    public function updateTransactionAction(Request $request, LoggerInterface $logger)
     {
         $data['transactionId'] = $request->get('transactionId');
         $data['amount'] = $request->get('amount');
+
+        if (empty($data['transactionId']) || empty($data['amount'])) {
+            $logger->error('Expected name and cnp not: ' . $data['transactionId'] . 'and' . $data['amount']);
+            throw new BadRequestHttpException
+            (
+                'Expected customerId and transactionId not: ' . $data['transactionId'] . 'and' . $data['amount']
+            );
+        }
 
         $em = $this->getDoctrine()->getManager();
         /**
@@ -111,21 +152,22 @@ class RestController extends FOSRestController
         $transaction->setAmount($data['amount']);
         $em->flush();
 
-        $view = $this->view(
-            [
-                'transactionId' => $data['transactionId'],
-                'customerId' => $transaction->getCustomerId(),
-                'amount' => $transaction->getAmount(),
-                'date' => $transaction->getDate()
-            ], 200
-        );
+        $output = [
+            'transactionId' => $data['transactionId'],
+            'customerId' => $transaction->getCustomerId(),
+            'amount' => $transaction->getAmount(),
+            'date' => $transaction->getDate()
+        ];
+
+        $view = $this->view($output, 200);
+        $logger->info("Update transaction success!" . PHP_EOL . $output);
         return $this->handleView($view);
     }
 
     /**
      * @Get("/transaction/delete/{transactionId}")
      */
-    public function deleteTransactionAction(Request $request)
+    public function deleteTransactionAction(Request $request, LoggerInterface $logger)
     {
         $data['transactionId'] = $request->get('transactionId');
 
@@ -141,17 +183,16 @@ class RestController extends FOSRestController
             );
         }
 
-        $em->remove($transaction);
-        $em->flush();
+        try {
+            $em->remove($transaction);
+            $em->flush();
+            $view = $this->view('success', 200);
+            $logger->info("Delete transaction success!" . PHP_EOL . $data['transactionId']);
+        } catch (\Exception $e) {
+            $view = $this->view("error", 400);
+            $logger->info("Delete transaction fail!" . PHP_EOL . $data['transactionId']);
+        }
 
-        $view = $this->view(
-            [
-                'transactionId' => $data['transactionId'],
-                'customerId' => $transaction->getCustomerId(),
-                'amount' => $transaction->getAmount(),
-                'date' => $transaction->getDate()
-            ], 200
-        );
         return $this->handleView($view);
     }
 
--- src/Isoft/RestApiBundle/Entity/Transaction.php
+++ src/Isoft/RestApiBundle/Entity/Transaction.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Isoft\RestApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
