e7997f73c0f8a987e7117a53d952a313be17a586
Lucian-Ionut <lucian.turiac@zitec.com>
2018-11-19 11:19:15 +0200

    Pagination, Limit, Filtering

--- src/Isoft/UserBundle/Controller/DefaultController.php
+++ src/Isoft/UserBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Isoft\UserBundle\Controller;
 
 use Isoft\RestApiBundle\Entity\Transaction;
+use Isoft\RestApiBundle\IsoftRestApiBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,15 +15,34 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $transactions = $this->getDoctrine()
-            ->getRepository(Transaction::class)->findAll();
 
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('IsoftRestApiBundle:Transaction')->createQueryBuilder('tr');
+
+        if($request->query->getAlnum('customerId')){
+            $queryBuilder
+                ->andWhere('tr.customerId LIKE :customerId')
+                ->setParameter('customerId', '%' . $request->query->getAlnum('customerId'). '%');
+        }
+        if($request->query->getAlnum('amount')){
+            $queryBuilder
+                ->andWhere('tr.amount LIKE :amount')
+                ->setParameter('amount', '%' . $request->query->getAlnum('amount'). '%');
+        }
+        if($request->query->getAlnum('date')){
+            $queryBuilder
+                ->andWhere('tr.date LIKE :date')
+                ->setParameter('date', '%' . $request->query->getAlnum('date'). '%');
+        }
+
+
+        $query = $queryBuilder->getQuery();
         /**
          * @var $paginator \Knp\Component\Pager\Paginator
          */
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-            $transactions,
+            $query,
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 5)
         );
